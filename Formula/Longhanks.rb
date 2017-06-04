048ada907b71e0e9ec184f31bbc708cf3c626d17
Andreas Schulz <andi.schulz@me.com>
2017-06-04 17:57:39 +0200

    SHA256 hash added

--- Formula/Longhanks.rb
+++ Formula/Longhanks.rb
@@ -2,7 +2,7 @@ class Longhanks < Formula
   desc "Meta formula for my desired formulae"
   homepage "https://github.com/Longhanks/homebrew-Longhanks"
   url "https://github.com/Longhanks/homebrew-Longhanks/archive/0.0.1.tar.gz"
-  sha256 ""
+  sha256 "69b572f65585d2c418bad84115016b7db80df93d691ea29da5b71a338fa57e53"
 
   depends_on "dcfldd"
   depends_on "htop"
