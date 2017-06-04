c5fdc0bcbec7c010f4a391bab42d6eda41b0488c
Andreas Schulz <andi.schulz@me.com>
2017-06-04 17:51:17 +0200

    Add meta formula Longhanks.rb

---
+++ Formula/Longhanks.rb
@@ -0,0 +1,23 @@
+class Longhanks < Formula
+  desc "Meta formula for my desired formulae"
+  homepage "https://github.com/Longhanks/homebrew-Longhanks"
+  url "https://github.com/Longhanks/homebrew-Longhanks/archive/0.0.1.tar.gz"
+  sha256 ""
+
+  depends_on "dcfldd"
+  depends_on "htop"
+  depends_on "most"
+  depends_on "neofetch"
+  depends_on "neovim/neovim/neovim"
+  depends_on "nmap"
+  depends_on "pyqt"
+  depends_on "python"
+  depends_on "python3"
+  depends_on "ruby"
+  depends_on "stow"
+  depends_on "watch"
+  depends_on "zsh"
+
+  def install
+  end
+end
